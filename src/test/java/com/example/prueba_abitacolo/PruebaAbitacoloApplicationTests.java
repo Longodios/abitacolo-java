326341abafeda891c0daf528986c64c84a27b535
Longodios <tusaromix@gmail.com>
2024-09-09 20:44:22 +0200

    First commit

---
+++ src/test/java/com/example/prueba_abitacolo/PruebaAbitacoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_abitacolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAbitacoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
